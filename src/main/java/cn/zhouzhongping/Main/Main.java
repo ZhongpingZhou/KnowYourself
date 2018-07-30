2ac558465a46e1b15940aa3404af6210536d21d2
ZhongpingZhou <18487376971@163.com>
2018-07-30 17:30:37 +0800

    elipse is not so friendly for developers

---
+++ src/main/java/cn/zhouzhongping/Main/Main.java
@@ -0,0 +1,10 @@
+package cn.zhouzhongping.Main;
+
+public class Main {
+	public static void main(String[]  args)
+	{
+		System.out.println("complain cant's save any thing,thinking and work can");
+		
+	}
+
+}
